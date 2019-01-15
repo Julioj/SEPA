821fb83e73e1b55b4d2ae48508b8bc8ae5ab2ed9
Julioj <jjmblanco23@gmail.com>
2019-01-15 19:17:38 -0300

    app/views/ordenes/_form.html.erb new show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190105152630) do
+ActiveRecord::Schema.define(version: 20190106175126) do
 
   create_table "ambitos", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -127,6 +127,7 @@ ActiveRecord::Schema.define(version: 20190105152630) do
     t.datetime "updated_at",   null: false
     t.integer  "companyId"
     t.string   "external_id"
+    t.string   "status"
   end
 
   create_table "solicitadors", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
